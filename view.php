c4e4440868300b07673a68d2b94393bd581dec12
Vavang <79837612+Vavang@users.noreply.github.com>
2021-03-01 18:24:58 +0100

    Update view.php

--- view.php
+++ view.php
@@ -6,3 +6,5 @@ Abscence faire liste joueur et gérer les abscence (enregistrer en csv ?)
 Faire en sorte que les boutons d'ajouts de personnes ou de match soit invisible par défaut et apparaissent lorsque l'on en a besoin en appuyant sur quelque chose par ex
 Amelioration graphique dans l'ensemble
 login ? pour administrateur (entraineur et secretaire) et les membres(joueurs)
+
+Penser à écrire le readme au fur et à mesure
