e72ca1ce7a455294746db09f4230c37a575c25d4
Vavang <79837612+Vavang@users.noreply.github.com>
2021-03-01 18:23:38 +0100

    Update view.php

--- view.php
+++ view.php
@@ -1,5 +1,8 @@
-<?php
+finir css pour effectif(Evan)
+Convocation(Evan ou Nathan)
+Match (Evan ou Nathan)
+Abscence faire liste joueur et gérer les abscence (enregistrer en csv ?)
 
-
-?>
-
+Faire en sorte que les boutons d'ajouts de personnes ou de match soit invisible par défaut et apparaissent lorsque l'on en a besoin en appuyant sur quelque chose par ex
+Amelioration graphique dans l'ensemble
+login ? pour administrateur (entraineur et secretaire) et les membres(joueurs)
