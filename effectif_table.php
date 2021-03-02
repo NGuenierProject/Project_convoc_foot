fd1a9ec93aa56b089318ed1972d79fffda4d85cb
Vavang <79837612+Vavang@users.noreply.github.com>
2021-03-02 10:04:18 +0100

    Update effectif_table.php

--- effectif_table.php
+++ effectif_table.php
@@ -1,16 +1,31 @@
+<style>
+	table,tr,th,td{
+		border: solid 1px;
+		border-collapse: collapse;
+		text-align: left;
+	}
+	th{
+		width:30%;
+	}
+	#letout #gauche {
+    	float:left;
+    	width:40%;
+	}
+</style>
 <?php
 $file = dirname(__FILE__) . "/effectif.csv";
 $i = 0;
 if (file_exists($file) && $id_file = fopen($file, "r")) {
-    echo "<table border=\"1\"><tbody>";
-    echo "<thead><tr><th>TYPE LICENCE</th><th>PRENOM, NOM</th></tr></thead>";
+	echo "<div id=\"gauche\">";    
+	echo "<table border=\"1\">";
+    echo "<tr><th>TYPE LICENCE</th><th>PRENOM, NOM</th></tr>";
     while ($tab = fgetcsv($id_file, 200, ";")) {
         $tlyc = $tab[1];
         $nom = $tab[2];
         echo "<tr><td>$tlyc</td><td>$nom</td></tr>";
         $i++;
     }
-    echo "</tbody></table>";
+    echo "</table> </div>";
     fclose($id_file);
 }
 $_SESSION["nb_licencie"]=$i;
