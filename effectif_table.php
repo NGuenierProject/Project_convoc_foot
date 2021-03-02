2c5fc97f1f4ee14db73d4efaacd8ea0d7b4f9685
Vavang <79837612+Vavang@users.noreply.github.com>
2021-03-02 14:38:45 +0100

    Update effectif_table.php

--- effectif_table.php
+++ effectif_table.php
@@ -1,14 +1,11 @@
 <?php
 $file = dirname(__FILE__) . "/effectif.csv";
-$i = 0;
 if (file_exists($file) && $id_file = fopen($file, "r")) {
     while ($tab = fgetcsv($id_file, 200, ";")) {
-        $tlic = $tab[1];
-        $nom = $tab[2];
+        $tlic = $tab[0];
+        $nom = $tab[1];
         echo "<tr><td>$tlic</td><td>$nom</td></tr>";
-        $i++;
     }
     fclose($id_file);
 }
-$_SESSION["nb_licencie"]=$i;
 ?>
