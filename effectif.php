b6a3e9176ca98ce8c380680a99eca8292b16bfd1
Vavang <79837612+Vavang@users.noreply.github.com>
2021-03-11 16:06:53 +0100

    Update effectif.php

--- effectif.php
+++ effectif.php
@@ -23,4 +23,24 @@
     catch(PDOException $e){
         echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
     }
+    $rien= "...";
+	$date_base='2021-08-01';
+	try{
+        	$dbco = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
+        	$dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		while($date_base <= '2022-07-31'){
+			$sth = $dbco->prepare("
+			    INSERT INTO absences(prenom_nom, date, motif)
+			    VALUES(:nom, :date_base,:rien)");
+			$sth->bindParam(':nom',$nom);
+			$sth->bindParam(':date_base',$date_base);
+			$sth->bindParam(':rien',$rien);
+		
+			$sth->execute();
+			$date_base = date('Y-m-d',strtotime('+7 day', strtotime($date_base)));
+		}
+	}
+	catch(PDOException $e){
+		echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
+	}	
 ?>
