e53eeb9bcd5fc48e234064868a57623fe8e2541c
Vavang <79837612+Vavang@users.noreply.github.com>
2021-03-19 18:13:19 +0100

    Delete Projet2 directory

--- Projet2/Controleur/controleur_absences.php
+++
@@ -1,9 +0,0 @@
-<?php
-try{
-	absences();
-}
-catch (Exception $e) {
-	header("Location:index.php?action=absences&erreur=1");
-}
-
-?>
--- Projet2/Controleur/controleur_convocation.php
+++
@@ -1,9 +0,0 @@
-<?php
-try{
-	convocation();
-}
-catch (Exception $e) {
-	header("Location:index.php?action=convocation&erreur=1");
-}
-
-?>
